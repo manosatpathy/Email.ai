9d94282ad5de34b61970e91e7f1e9211503b8261
manosatpathy <manosatpathy@gmail.com>
2024-06-09 00:05:09 +0530

    initial commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSession, signIn, signOut } from 'next-auth/react'
+
+const Login = () => {
+  return (
+    <div>
+      login
+    </div>
+
+
+  )
+}
+
+export default Login
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='border border-red-500'>
+            <div>
+                <div>DP</div>
+                <div>
+                    <div>Name</div>
+                    <div>Email</div>
+                </div>
+            </div>
+            <div>
+                <button>Logout</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
